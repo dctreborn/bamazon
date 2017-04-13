6ffbdea7320388a6808f3eeaf12b27e613daff11
Rukaroa <rukaroa@gmail.com>
2017-04-13 15:47:26 -0500

    to troubleshoot

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,25 +1,14 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var password = require("./db.js").db;
 
-var connection;
-
-inquirer.prompt([{
-    message: "Enter database password",
-    name: "password",
-    type: "password"
-}]).then(function(db) {
-
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: db.password,
-        database: "Bamazon"
-    });
-
-    main();
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: password,
+    database: "Bamazon"
 });
-
 
 //RPG themed bamazon
 
@@ -33,7 +22,7 @@ function main() {
 
         switch (user.option) {
             case "Buy Items":
-                buy();
+                menu();
                 break;
             default:
                 console.log("Goodbye.");
@@ -42,7 +31,7 @@ function main() {
     });
 }
 
-function buy() {
+function menu() {
     var query = "SELECT * FROM products";
     var items = [];
 
@@ -60,26 +49,82 @@ function buy() {
                 ", Stock: " + item.stock_quantity);
         }
 
-        inquirer.prompt([{
+        buyItem(items);
+    });
+}
+
+function buyItem(inventory) {
+
+	inquirer.prompt([{
             type: "list",
             message: "What do you want to buy?",
-            choices: items,
+            choices: inventory,
             name: "item"
         }]).then(function(buy) {
+        	//slice item number from given string and UPDATE products SET stock_quantity = stock_quantity - bought WHERE item_id = id
+        	var item = buy.item;
+        	var id = item.slice(item.indexOf("#") + 1, item.indexOf(":"));
+        	var price = item.slice(item.indexOf("Price") + 8, item.indexOf(", Stock"));
+
         	//ask amount to buy
-            //if amount to buy <= stock, process order and calc price
-            //else, display not enough and bring back to main menu
-            //slice item number from given string and UPDATE products SET stock_quantity = stock_quantity - bought WHERE item_id = id
-            console.log(buy);
+        	inquirer.prompt([
+        	{
+        		type: "input",
+        		message: "How many do you want to buy?",
+        		name: "bought",
+        		validate: function (input) {
+				    // Declare function as asynchronous, and save the done callback 
+				    var done = this.async();
+				 
+				    // Do async stuff 
+				    setTimeout(function () {
+				      if (typeof input !== 'number') {
+				        // Pass the return value in the done callback 
+				        done('You need to provide a number');
+				        return;
+				      }
+				      // Pass the return value in the done callback 
+				      done(null, true);
+				    }, 3000);
+				  }
+        	}
+        	]).then(function(stock){
+        		var numBought = Math.max(stock.bought, 0); //cannot have value less than 0
+        		
+           		checkStock(numBought, id, price); 		
+
+        	});            
         });
-    });
-
 }
 
-function checkStock() {
+//if amount to buy <= stock, process order and calc price
+//else, display not enough and bring back to main menu
+function checkStock(sold, id, price) {
 
+	var query = "SELECT * FROM products WHERE ?";
+
+	connection.query(query, [{item_id: id}], function(err, results) {
+		if (err) throw err;
+
+		var item = results.product_name;
+
+		if (results.stock_quantity < sold) {
+			console.log("Not enough in stock.");
+			console.log("Try again later.");
+			main();
+		} else {
+			var query = "UPDATE products SET ? WHERE ?";
+
+			connection.query(query, [{stock_quantity: stock_quantity - sold, item_id: id}], function(err, results) {
+				//calcuate cost
+				var total = sold * price;
+
+				console.log("Bought %s %s for %i gold.", sold, item, total);
+			});
+
+		}
+	});
 }
 
-function calcPrice() {
-
-}
+//start program
+main();
