844789e9b16f0242b1d158a48d178fa6dc4b1f3e
rukaroa <rukaroa@gmail.com>
2017-04-13 19:54:32 -0500

    formatted messages. clarified database password errors. readme updated

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # bamazon
+Bamazon is a node app that mimics a marketplace where you can buy items based on a given stock.
+
+# Instructions
+*Set up the database in MySQL using the commands found in bamazon.sql
+*Run bamazonCustomer.js in node after installing dependencies
+*Type in your database password to use the app (required)
+*Select an item from the drop down and an amount to buy
+*Stock will decrease and retain value with each program call
+
+# Dependencies
+inquirer
+
+mysql
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -46,76 +46,85 @@ function menu() {
 
     //build item array with prices and stock
     connection.query(query, function(err, results) {
-        if (err) throw err;
+        if (err) {
+            console.log("-----");
+            console.log("Incorrect database password. Please try again.");
+            console.log("-----");
+        } else {
 
-        var length = results.length;
+            var length = results.length;
 
-        for (var i = 0; i < length; i++) {
-            var item = results[i];
-            items.push("#" + item.item_id + ": " +
-                item.product_name +
-                ", Price: " + item.price +
-                ", Stock: " + item.stock_quantity);
+            for (var i = 0; i < length; i++) {
+                var item = results[i];
+                items.push("#" + item.item_id + ": " +
+                    item.product_name +
+                    ", Price: " + item.price +
+                    ", Stock: " + item.stock_quantity);
+            }
+
+            buyItem(items);
         }
-
-        buyItem(items);
     });
 }
 
 function buyItem(inventory) {
 
-	inquirer.prompt([{
-            type: "list",
-            message: "What do you want to buy?",
-            choices: inventory,
-            name: "item"
-        }]).then(function(buy) {
-        	//slice item number from given string and UPDATE products SET stock_quantity = stock_quantity - bought WHERE item_id = id
-        	var item = buy.item;
-        	var id = parseInt(item.slice(item.indexOf("#") + 1, item.indexOf(":")));
-        	var price = parseInt(item.slice(item.indexOf("Price") + 7, item.indexOf(", Stock")));
+    inquirer.prompt([{
+        type: "list",
+        message: "What do you want to buy?",
+        choices: inventory,
+        name: "item"
+    }]).then(function(buy) {
+        //slice item number from given string and UPDATE products SET stock_quantity = stock_quantity - bought WHERE item_id = id
+        var item = buy.item;
+        var id = parseInt(item.slice(item.indexOf("#") + 1, item.indexOf(":")));
+        var price = parseInt(item.slice(item.indexOf("Price") + 7, item.indexOf(", Stock")));
 
-        	//ask amount to buy
-        	inquirer.prompt([
-        	{
-        		type: "input",
-        		message: "How many do you want to buy?",
-        		name: "bought"
-        	}
-        	]).then(function(stock){
-        		var numBought = Math.max(stock.bought, 0); //cannot have value less than 0
-        		
-           		checkStock(numBought, id, price); 		
+        //ask amount to buy
+        inquirer.prompt([{
+            type: "input",
+            message: "How many do you want to buy?",
+            name: "bought"
+        }]).then(function(stock) {
+            var numBought = Math.max(stock.bought, 0); //cannot have value less than 0
 
-        	});            
+            checkStock(numBought, id, price);
+
         });
+    });
 }
 
 //if amount to buy <= stock, process order and calc price
 //else, display not enough and bring back to main menu
 function checkStock(sold, id, price) {
-	var query = "SELECT * FROM products WHERE item_id = ?";
+    var query = "SELECT * FROM products WHERE item_id = ?";
 
-	connection.query(query, [id], function(err, results) {
-		if (err) throw err;
+    connection.query(query, [id], function(err, results) {
+        if (err) throw err;
 
-		var item = results[0].product_name;
-		var stock = results[0].stock_quantity;
+        var item = results[0].product_name;
+        var stock = results[0].stock_quantity;
 
-		if (stock < sold) {
-			console.log("Not enough in stock.");
-			console.log("Try again later.");
-			main();
-		} else {
-			var query = "UPDATE products SET stock_quantity = ? WHERE item_id = ?";
+        if (stock < sold) {
+            console.log("-----");
+            console.log("Not enough in stock.");
+            console.log("Try again later.");
+            console.log("-----");
+            main();
+        } else {
+            var query = "UPDATE products SET stock_quantity = ? WHERE item_id = ?";
 
-			connection.query(query, [(stock - sold), id], function(err, results) {
-				//calcuate cost
-				var total = sold * price;
+            connection.query(query, [(stock - sold), id], function(err, results) {
+                //calcuate cost
+                var total = sold * price;
 
-				console.log("Bought %s %s for %s gold.", sold, item, total);
-			});
+                console.log("-----");
+                console.log("Bought %s %s for %s gold.", sold, item, total);
+                console.log("-----");
 
-		}
-	});
+                main()
+            });
+
+        }
+    });
 }
